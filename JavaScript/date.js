bf3ca4ab04c634adc02bac7b2ff9f4d22cb5ced4
Altered_Taal <145328957+Laateu@users.noreply.github.com>
2024-06-12 22:50:46 +0900

    Update date.js

--- JavaScript/date.js
+++ JavaScript/date.js
@@ -1,6 +1,11 @@
-document.getElementByID("date").innerHTML = showTime();
+document.getElementByID("date").innerHTML += showTime();
 
 function showTime(){
-  let date = new Date();
-  return date;
+  let dates = new Date();
+  let month = dates.getMonth() + 1;
+  let date = dates.getDate();
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  let day = dates.getDay();
+  message = "今日は" + month + "/" + day + "(" + days[day] + ")です．";
+  return message;
 }
