b7d7c0b15d30efdacb02ef157d03e3aa21563fe5
Altered_Taal <145328957+Laateu@users.noreply.github.com>
2024-06-12 23:40:00 +0900

    Create greeting.js

---
+++ JavaScript/greeting.js
@@ -0,0 +1,15 @@
+let element = document.getElementById("greet");
+element.innerHTML = greeting();
+
+function greeting(){
+  let dates = new Date();
+  let hour = dates.getHours();
+  let message = "こんばんは！"
+  if(hour >= 4 && hour <= 9){
+    message = "おはようございます！"
+  }
+  else if(hour > 9 && hour <= 18){
+    message = "こんにちは！"
+  }
+  return message;
+}
