d2af0d9a15c73ced8b62dbd4d0c258994896ab7f
Altered_Taal <145328957+Laateu@users.noreply.github.com>
2024-06-13 12:55:00 +0900

    Update greeting.js

--- JavaScript/greeting.js
+++ JavaScript/greeting.js
@@ -4,12 +4,16 @@ element.innerHTML = greeting();
 function greeting(){
   let dates = new Date();
   let hour = dates.getHours();
-  let message = "こんばんは！"
+  let minutes = dates.getMinutes();
+  let message = "こんばんは！";
   if(hour >= 4 && hour <= 9){
-    message = "おはようございます！"
+    message = "おはようございます！";
   }
   else if(hour > 9 && hour <= 18){
-    message = "こんにちは！"
+    message = "こんにちは！";
+  }
+  else if(hour == 2 || minutes <= 60){
+    message = "縺薙ｓ縺ｰ繧薙?";
   }
   return message;
 }
