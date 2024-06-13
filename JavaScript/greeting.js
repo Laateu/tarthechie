2ad902bb1a2cb54f23436abcdf329359e6368f30
Altered_Taal <145328957+Laateu@users.noreply.github.com>
2024-06-14 08:38:05 +0900

    Update greeting.js

--- JavaScript/greeting.js
+++ JavaScript/greeting.js
@@ -9,7 +9,7 @@ function greeting(){
   if(hour >= 4 && hour <= 9){
     message = "おはようございます！";
   }
-  else if(hour > 9 && hour <= 18){
+  else if(hour > 9 && hour < 18){
     message = "こんにちは！";
   }
   else if(hour == 2 && minutes <= 30){
